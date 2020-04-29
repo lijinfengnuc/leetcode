5ff903910dda673cb3ddd88ca0767078a9461607
lijinfeng <1205626806@qq.com>
2020-04-29 11:11:36 +0800

    feat:完成Q10

--- src/nuc/ljf/leetcode/Q10.java
+++ src/nuc/ljf/leetcode/Q10.java
@@ -17,14 +17,12 @@ public class Q10 {
 
     public static void main(String[] args) {
         Q10 q = new Q10();
-        System.out.println(q.isMatch("aa","a"));
+        System.out.println(q.isMatch("ab", ".*c"));
     }
 
     public boolean isMatch(String s, String p) {
-        boolean sEmpty = s.isEmpty();
-        boolean pEmpty = p.isEmpty();
-        if(sEmpty || pEmpty) {
-            return sEmpty && pEmpty;
+        if(p.isEmpty()) {
+            return s.isEmpty();
         }
         if(p.equals(".*")) {
             return true;
@@ -35,41 +33,33 @@ public class Q10 {
         char[] pChars = p.toCharArray();
         /*dp[i][j]表示s[0,i]与p[0,j]匹配*/
         boolean[][] dp = new boolean[sLen+1][pLen+1];
-        if(sChars[0] != pChars[0] && pChars[0] != '.') {
-            return false;
-        }
-        dp[1][1] = true;
-        for(int i=0; i < sLen + 1; i++) {
-            for(int j=0; j < pLen + 1; j++) {
-                if(i == 0 || j == 0) {
-                    dp[i][j] = true;
-                }
-                if(dp[i-1][j-1]) {
-                    if(sChars[i+1] == pChars[j+1]) {
+        dp[0][0] = true;
+        for(int i=0; i < sLen+1; i++) {
+            //dp[i][0]一定是false，无需匹配，j从1开始
+            for(int j=1; j < pLen+1; j++) {
+                if(pChars[j-1] == '*') {
+                    //p[j-1]为*
+                    //匹配0个
+                    if(dp[i][j-2]) {
                         dp[i][j] = true;
                         continue;
                     }
-                    if(pChars[j+1] == '.') {
-                        dp[i][j] = true;
-                        continue;
+                    //匹配1个或者多个
+                    if(i > 0 && (sChars[i-1] == pChars[j-2] || pChars[j-2] == '.')) {
+                        //s[i-1]与p[j-2]匹配
+                        //1个 或者 多个
+                        dp[i][j] = dp[i-1][j-1] || dp[i-1][j];
                     }
-                    if(sChars[i+1] == pChars[j-1] && pChars[j] == '*')
-
-                }
-
-
-                if(sChars[i+1] == pChars[j+1] || pChars[j+1] == '.' || (sChars[i+1] == pChars[j-1] && pChars[j] == '*')) {
-                    dp[i+1][j+1] = true;
-                }
-                if(j > 0 && (sChars[i+1] == pChars[j-1] && pChars[j] == '*')) {
-                    dp[i+1][j] = true;
-                }
-                if(pChars[j] == '*') {
-                    //j加1
-                    dp[i][j+1] = true;
+                }else {
+                    //p[j-1]不为*
+                    //匹配1个
+                    if(i > 0 && (sChars[i-1] == pChars[j-1] || pChars[j-1] == '.')) {
+                        dp[i][j] = dp[i-1][j-1];
+                    }
                 }
             }
         }
+        return dp[sLen][pLen];
     }
 
 }
