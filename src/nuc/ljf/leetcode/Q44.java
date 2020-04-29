47ec9e7476d37bbfa85b33ed9a5ae90b8bcf0aa6
lijinfeng <1205626806@qq.com>
2020-04-29 18:44:36 +0800

    feat:添加Q44

---
+++ src/nuc/ljf/leetcode/Q44.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 1993, 2020, the original author or authors. All rights reserved.
+ */
+
+package nuc.ljf.leetcode;
+
+/**
+ * ClassName: Q44 <br/>
+ * Function: <br/>
+ * Date: 2020年04月29日 18:43 <br/>
+ *
+ * @author lijinfeng
+ */
+
+public class Q44 {
+
+    public static void main(String[] args) {
+        Q44 q = new Q44();
+        System.out.println(q.isMatch("aa","a"));
+    }
+
+    public boolean isMatch(String s, String p) {
+        return true;
+    }
+
+}
