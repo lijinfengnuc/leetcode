9d91133ecacf0ee5f2d8a332227fa2c559a99d49
lijinfeng <1205626806@qq.com>
2020-04-30 19:35:39 +0800

    feat:完成Q85

--- src/nuc/ljf/leetcode/Q85.java
+++ src/nuc/ljf/leetcode/Q85.java
@@ -26,8 +26,39 @@ public class Q85 {
     }
 
     public int maximalRectangle(char[][] matrix) {
-        
-        return 0;
+        int max = 0;
+        int m = matrix.length;
+        if(m == 0) {
+            return max;
+        }
+        int n = matrix[0].length;
+        if(n == 0) {
+            return max;
+        }
+        //dp[i][j]表示第i行中以matrix[i][j]结尾最长连续1的个数
+        int[][] dp = new int[m][n];
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                //matrix[i][j]为'0'直接跳过，一定不会有以'0'为右下角的矩形
+                if(matrix[i][j] == '0') {
+                    continue;
+                }
+                //获取dp[i][j]的值
+                dp[i][j] = j==0 ? 1: dp[i][j-1] + 1;
+                //获取当前行的面积
+                int width = dp[i][j];
+                max = Math.max(max, width);
+                for(int k=i-1; k>=0; k--) {
+                    //从第i行往上倒推，获取每个以matrix[i][j]为右下角的矩形的面积
+                    if(dp[k][j] == 0) {
+                        break;
+                    }
+                    width = Math.min(width, dp[k][j]);
+                    max = Math.max(max, width*(i-k+1));
+                }
+            }
+        }
+        return max;
     }
 
 }
