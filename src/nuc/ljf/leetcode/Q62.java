d6f9ca223abcc5176b6f2a52f775d07c73cc2ee2
lijinfeng <1205626806@qq.com>
2020-04-30 11:58:25 +0800

    doc:Q62添加注释

--- src/nuc/ljf/leetcode/Q62.java
+++ src/nuc/ljf/leetcode/Q62.java
@@ -28,8 +28,10 @@ public class Q62 {
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(i==0 || j==0) {
+                    //第一行、第一列只可能是1
                     dp[i][j] = 1;
                 }else {
+                    //当前路径条数=左侧路径条数+上方路径条数
                     dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
