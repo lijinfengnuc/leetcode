2ffd2f3f6bcfb680b98637b636565bc356caf4d7
lijinfeng <1205626806@qq.com>
2020-05-12 18:44:46 +0800

    feat:添加Q115

---
+++ src/nuc/ljf/leetcode/Q115.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 1993, 2020, the original author or authors. All rights reserved.
+ */
+
+package nuc.ljf.leetcode;
+
+/**
+ * ClassName: Q115 <br/>
+ * Function: <br/>
+ * Date: 2020年05月12日 18:43 <br/>
+ *
+ * @author lijinfeng
+ */
+
+public class Q115 {
+
+    public static void main(String[] args) {
+        Q115 q = new Q115();
+        System.out.println(q.numDistinct("", ""));
+    }
+
+    public int numDistinct(String s, String t) {
+        return 0;
+    }
+
+}
